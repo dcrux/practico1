2e0f54e06909d6754f3a634b21d8bf799f905667
cruxdavid <crux@unitec.edu>
2015-11-11 23:07:42 -0600

    create producto CRUD

---
+++ db/migrate/20151112050428_create_productos.rb
@@ -0,0 +1,11 @@
+class CreateProductos < ActiveRecord::Migration
+  def change
+    create_table :productos do |t|
+      t.string :nombre
+      t.float :precio
+      t.text :descripcion
+
+      t.timestamps null: false
+    end
+  end
+end
